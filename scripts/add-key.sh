177da4e693f07506fee25cc13c3bb9e843de4755
Alex Belozierov <oleksandr.belozierov@kindgeek.com>
2019-09-25 01:28:58 +0300

    - update travis

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-security create-keychain -p travis ios-build.keychain
+KEY_CHAIN=ios-build.keychain
+security create-keychain -p travis $KEY_CHAIN
+security default-keychain -s $KEY_CHAIN
+security unlock-keychain -p travis $KEY_CHAIN
+security set-keychain-settings -t 3600 -u $KEY_CHAIN
+
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/dev.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+
+echo "list keychains: "
+security list-keychains
+echo " ****** "
+
+echo "find indentities keychains: "
+security find-identity -p codesigning  ~/Library/Keychains/ios-build.keychain
+echo " ****** "
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
